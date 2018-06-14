d27b93d4933f0e428e9be3137234cf1ba015b45a
Md Shakil Ahmed <eng.shakil8838@gmail.com>
2018-06-14 16:35:59 +0600

    minor UI changed

--- FlexiPlan/src/main/java/com/example/flexiplan/MainActivity.java
+++ FlexiPlan/src/main/java/com/example/flexiplan/MainActivity.java
@@ -79,17 +79,4 @@ public class MainActivity extends Activity {
         webview.loadUrl("http://www.grameenphone.com/flexi-plan/");
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        /*getMenuInflater().inflate(R.menu.main, menu);*/
-        
-        ActionBar actionBar = getActionBar();
-        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#2CAEAA")));
-        /*actionBar.setHomeButtonEnabled(true);*/
-        
-        return true;
-    }
-      
 }
